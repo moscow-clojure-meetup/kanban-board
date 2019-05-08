4bee7cbfa0775addf1360913d16d11cd632ca5cf
Sergey Tkachenko <sergey.tkachenko@flexiana.com>
2019-05-08 11:55:47 +0300

    Add domain entities description

---
+++ src/main/kanban/model/board.cljs
@@ -0,0 +1,7 @@
+(ns kanban.model.board)
+
+
+(comment
+ {:id   #uuid "163b2480-9c03-4e98-969b-b6e214978a8a"
+  :name "Board name"})
+  
---
+++ src/main/kanban/model/card.cljs
@@ -0,0 +1,8 @@
+(ns kanban.model.card)
+
+
+(comment
+ {:id          #uuid "4f426e5c-6f82-4cb1-9a33-f3f30a0e628e"
+  :name        "Card name"
+  :description "Card description"
+  :created     #inst "2019-05-08T08:22:37.468-00:00"})
